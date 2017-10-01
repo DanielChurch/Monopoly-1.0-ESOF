7e0781267a18f3fe2c18d0f3f2d4d68614023d3a
danielchurch <daniellchurch@fvcc.edu>
2017-10-01 04:08:42 -0600

    Added equals and magnitude

--- src/esof322/a1/Vector3D.java
+++ src/esof322/a1/Vector3D.java
@@ -20,12 +20,21 @@ public final class Vector3D {
 
     public double dot(Vector3D other) {return 0d;}
 
-    public double magnitude() {return 0d;}
+    public double magnitude() {
+        return Math.sqrt(dot(this));
+    }
 
     @Override
     public String toString() {return "";}
 
     @Override
-    public boolean equals(Object other) {return false;}
+    public boolean equals(Object other) {
+        if(other instanceof Vector3D) {
+            Vector3D otherVector = (Vector3D) other;
+            return subtract(otherVector).magnitude() <= 0.1d; // TODO: Consider refining tolerance
+        } else {
+            throw new IllegalArgumentException("You can only compare a vector to another vector.");
+        }
+    }
 
 }
--- test/esof322/a1/Vector3DTest.java
+++ test/esof322/a1/Vector3DTest.java
@@ -10,8 +10,23 @@ import static org.junit.Assert.*;
 
 public class Vector3DTest {
 
+    @Rule
+    public final ExpectedException invalidArgumentException = ExpectedException.none();
+
+    Vector3D vector101;
+    Vector3D vector010;
+
+    Vector3D vector541;
+    Vector3D vector236;
+
     @Before
-    public void setUp() throws Exception {}
+    public void setUp() throws Exception {
+        vector101 = new Vector3D(1, 0, 1);
+        vector010 = new Vector3D(0, 1, 0);
+
+        vector541 = new Vector3D(5,4,1);
+        vector236 = new Vector3D(2,3,6);
+    }
 
     @Test
     public void testAdd() throws Exception {}
@@ -29,11 +44,33 @@ public class Vector3DTest {
     public void testDot() throws Exception {}
 
     @Test
-    public void testMagnitude() throws Exception {}
+    public void testMagnitude() throws Exception {
+        assertEquals(1d, vector010.magnitude(), 0d);
+        assertEquals(1.414d, vector101.magnitude(), 0.001d);
+        assertEquals(6.48d, vector541.magnitude(), 0.01d);
+        assertEquals(7d, vector236.magnitude(), 0d);
+    }
 
     @Test
     public void testToString() throws Exception {}
 
     @Test
-    public void testEquals() throws Exception {}
+    public void testEquals() throws Exception {
+        assertTrue(new Vector3D(1,0,1).equals(new Vector3D(1,0,1)));
+        assertTrue(vector010.equals(vector010));
+        assertFalse(vector010.equals(vector541));
+
+        assertTrue(new Vector3D(1,0,0).equals(new Vector3D(1,0,0)));
+        assertFalse(new Vector3D(1,0,0).equals(new Vector3D(0,0,0)));
+
+        assertTrue(new Vector3D(0,1,0).equals(new Vector3D(0,1,0)));
+        assertFalse(new Vector3D(0,1,0).equals(new Vector3D(0,0,0)));
+
+        assertTrue(new Vector3D(0,0,1).equals(new Vector3D(0,0,1)));
+        assertFalse(new Vector3D(0,0,1).equals(new Vector3D(0,0,0)));
+
+        invalidArgumentException.expect(IllegalArgumentException.class);
+        invalidArgumentException.expectMessage("You can only compare a vector to another vector.");
+        vector010.equals(0d);
+    }
 }
