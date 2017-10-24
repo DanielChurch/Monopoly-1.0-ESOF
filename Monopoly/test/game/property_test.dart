9b8f248bef7387819ba96484da803ee9589c33af
danielchurch <daniellchurch@fvcc.edu>
2017-10-23 22:41:39 -0600

    Actually add files to commit

---
+++ Monopoly/test/game/property_test.dart
@@ -0,0 +1,13 @@
+
+import 'package:Monopoly/game/property.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Property', () {
+    Property property;
+
+    setUp(() {
+      property = new Property();
+    });
+  });
+}
