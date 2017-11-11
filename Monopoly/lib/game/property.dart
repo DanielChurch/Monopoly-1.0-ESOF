461a85da7e7f270f07c88e49b903455c2660c6f9
Daniel Church <daniellchurch@fvcc.edu>
2017-11-10 21:39:08 -0700

    update properties (#28)

--- Monopoly/lib/game/property.dart
+++ Monopoly/lib/game/property.dart
@@ -1,17 +1,35 @@
+
+enum Color {
+  brown,
+  lightBlue,
+  purple,
+  orange,
+  red,
+  yellow,
+  green,
+  darkBlue,
+  utility,
+  railroad,
+}
+
 class Property {
 
-  int _rent;
+  List _rent;
+
+
   bool _isMortgaged = false;
 
   bool isOwned = false;
   int price;
-  String color;
+  int mortgage;
+  Color color;
+
   int numHouses = 0;
   int numHotels = 0;
 
-  Property(int this.price, int this._rent, String this.color);
+  Property(int this.price, int this.mortgage, List this._rent, Color this.color);
 
   bool get isMortgaged => _isMortgaged;
-  int get rent => _rent;
+  List get rent => _rent;
 
 }
